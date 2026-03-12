fb7e1da0aedfa974838ecdd7b021215fdc2a834f
Vardhan0609 <vardhanreddypuli06@gmail.com>
2026-03-12 15:52:22 +0530

    5th commit

--- test_model.py
+++ test_model.py
@@ -1,4 +1,8 @@
-from calculator import add
+def add(a, b):
+    return a + b
 def test_add():
-    result = add(2, 3)
-    assert result == 5
+    assert add(2, 3) == 5
+def test_add_negative():
+    assert add(-1, -2) == -3
+def test_add_zero():
+    assert add(0, 5) == 5
